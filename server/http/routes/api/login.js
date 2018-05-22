65979ee7df788f281f7ef941f3228d9537e37f02
DJ <djilanov@gmail.com>
2018-05-22 10:19:37 -0400

    Add api endpoints

---
+++ server/http/routes/api/login.js
@@ -0,0 +1,9 @@
+module.exports = function (plasma, dna, helpers) {
+    return {
+      'GET': function (req, res, next) {
+        res.body = require(process.cwd() + '/package.json').version
+        next()
+      }
+    }
+  }
+  
